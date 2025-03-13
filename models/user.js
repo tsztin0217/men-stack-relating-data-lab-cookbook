808fd8ec9e1fb27a355c0ce9e5bddffdb2ed967f
tsztin0217 <tsztin0217@gmail.com>
2025-03-12 23:59:12 -0400

    created foodSchema and embedded it within userSchema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
+const foodSchema = new mongoose.Schema({
+  name: String,
+  required: true,
+})
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +14,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  pantry: [foodSchema]
 });
 
 const User = mongoose.model('User', userSchema);
