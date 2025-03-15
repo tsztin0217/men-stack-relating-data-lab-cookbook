e7e86241c264df5e5803f457b94963ef263ad944
tsztin0217 <tsztin0217@gmail.com>
2025-03-14 21:48:52 -0400

    Create delete route for food items

--- controllers/foods.js
+++ controllers/foods.js
@@ -31,4 +31,19 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.delete('/:itemId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.pantry.id(req.params.itemId).deleteOne();
+
+        await currentUser.save()
+        res.redirect(`/users/${currentUser._id}/foods`)
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+
+
+} )
+
 module.exports = router;
