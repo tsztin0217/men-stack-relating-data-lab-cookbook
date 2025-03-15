587b443e4069bf30385032579924eb766737e1bd
tsztin0217 <tsztin0217@gmail.com>
2025-03-14 21:05:32 -0400

    changed to the correct route for get

--- controllers/foods.js
+++ controllers/foods.js
@@ -3,13 +3,32 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-router.get('/', (req, res) => {
-    res.render('foods/index.ejs');
+router.get('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        res.render('foods/index.ejs', {
+           pantry: currentUser.pantry
+        })
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
 })
 
-router.get('/new', (req, res) => {
+router.get('/new', async (req, res) => {
     res.render('foods/new.ejs');
 })
 
+router.post('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.pantry.push(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/foods`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+})
 
 module.exports = router;
